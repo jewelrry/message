e0fa32f87d2169ddacbdd27a65006d838ef49cf8
zhourui <2228097305@qq.com>
2023-04-06 11:18:57 +0800

    test

--- example/main.go
+++ example/main.go
@@ -1,32 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"github.com/jewelrry/message"
-	"github.com/jewelrry/message/domain"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-)
-
 func main() {
-	dsn := "root:1astWeekend@tcp(127.0.0.1)/test?charset=utf8mb4&parseTime=True&loc=Local"
-	mysqlConfig := mysql.Config{
-		DSN:               dsn, // DSN data source name
-		DefaultStringSize: 191, // string 类型字段的默认长度
-	}
-
-	db, err := gorm.Open(mysql.New(mysqlConfig))
-	if err != nil {
-		fmt.Printf("err:%+v \n", err)
-	}
-
-	base := domain.MessageBase{
-		ReceiverRange: 1,
-	}
-	req := domain.MessageReq{
-		MessageBase: base,
-	}
-	c, err := message.NewMessageClient(db)
-	id, err := c.Create(req)
-	fmt.Printf("id:%+v, err:%+v \n", id, err)
+	//dsn := "root:1astWeekend@tcp(127.0.0.1)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	//mysqlConfig := mysql.Config{
+	//	DSN:               dsn, // DSN data source name
+	//	DefaultStringSize: 191, // string 类型字段的默认长度
+	//}
+	//
+	//db, err := gorm.Open(mysql.New(mysqlConfig))
+	//if err != nil {
+	//	fmt.Printf("err:%+v \n", err)
+	//}
+	//
+	//base := domain.MessageBase{
+	//	ReceiverRange: 1,
+	//}
+	//req := domain.MessageReq{
+	//	MessageBase: base,
+	//}
+	//c, err := message.NewMessageClient(db)
+	//id, err := c.Create(req)
+	//fmt.Printf("id:%+v, err:%+v \n", id, err)
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.17
 require (
 	github.com/facebookgo/inject v0.0.0-20180706035515-f23751cae28b
 	github.com/snowlyg/helper v0.1.42
-	gorm.io/driver/mysql v1.4.7
 	gorm.io/gorm v1.24.6
 )
 
@@ -15,7 +14,6 @@ require (
 	github.com/facebookgo/stack v0.0.0-20160209184415-751773369052 // indirect
 	github.com/facebookgo/structtag v0.0.0-20150214074306-217e25fb9691 // indirect
 	github.com/facebookgo/subset v0.0.0-20200203212716-c811ad88dec4 // indirect
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 )
